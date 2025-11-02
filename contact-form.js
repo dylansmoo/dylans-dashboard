94e4df7c118f75b41082e40d5b92fdbc227cc3a7
Dylan <dylansmouha@gmail.com>
2025-11-02 17:03:56 +1100

    some more minor styling changes

--- contact-form.js
+++ contact-form.js
@@ -1,9 +1,32 @@
+const toggleBtn = document.getElementById("toggleThemeBtn");
+const themeIcon = document.getElementById("themeIcon");
+const body = document.body;
+
+const savedTheme = localStorage.getItem("pageTheme");
+
+if (savedTheme === "dark") {
+  body.classList.add("dark");
+  themeIcon.textContent = "☀️";
+} else {
+  themeIcon.textContent = "🌙";
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const currTheme = body.classList.contains("dark") ? "dark" : "light";
+  localStorage.setItem("pageTheme", currTheme);
+  themeIcon.textContent = currTheme === "dark" ? "☀️" : "🌙";
+});
+
+
+
+
 //contact form JS
 const form = document.querySelector("#contactForm")
 const sendStatus = document.querySelector("#status")
 
-form.addEventListener("submit", () => {
-  event.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
